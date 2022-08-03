b9c979ddb1c6e7c998efe74c5b899455709224f2
Md. Saif Ahammod Khan <saif.ahammod@northsouth.edu>
2022-08-04 02:07:19 +0600

    Image addition

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,16 +1,21 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           backgroundColor: Colors.blueGrey[900],
-          title: Text('I am Rich'),
+          title: Center(
+            child: const Text('I AM RICH'),
+          ),
         ),
-        body:,
+        body: Center(
+          child: Image(
+            image: AssetImage("images/diamond.png"),
+          ),
+        ),
       ),
     ),
   );
