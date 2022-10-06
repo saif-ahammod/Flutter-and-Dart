a4744833f265b82637de4e885bd094ea438bb043
Md. Saif Ahammod Khan <saif.ahammod@northsouth.edu>
2022-10-07 00:15:39 +0600

    Commenting out for MI card

--- mi_card_saif/lib/main.dart
+++ mi_card_saif/lib/main.dart
@@ -12,114 +12,115 @@ class myapp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: SafeArea(
-          child: Column(
-            // mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            //crossAxisAlignment: CrossAxisAlignment.center, //verticalDirection: VerticalDirection.up,
-            verticalDirection: VerticalDirection.down,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    child: Text("Container R1"),
-                    height: 100,
-                    width: 100,
-                    //width:double.infinity
-                    color: Colors.cyan,
-                    //margin: EdgeInsets.all(50.0),
-                    //margin: EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0)
-                    //margin: EdgeInsets.only(left: 50),
-                    //margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                    //padding: EdgeInsets.all(20),//Layout widget(ROW COLUMN and Stack)
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Container(
-                    child: Text("Container R2"),
-                    height: 100,
-                    width: 100,
-                    color: Colors.pink,
-                    //margin: EdgeInsets.all(50.0),
-                    //margin: EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0)
-                    //margin: EdgeInsets.only(left: 50),
-                    //margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                    //padding: EdgeInsets.all(20),
-                    //Layout widget(ROW COLUMN and Stack)
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Container(
-                    child: Text("Container 1"),
-                    height: 100,
-                    width: 100,
-                    color: Colors.orange,
-                    //margin: EdgeInsets.all(50.0),
-                    //margin: EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0)
-                    //margin: EdgeInsets.only(left: 50),
-                    //margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                    //padding: EdgeInsets.all(20),
-                    //Layout widget(ROW COLUMN and Stack)
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                child: Text("Container 1"),
-                height: 100,
-                width: 200,
-                //width:double.infinity
-                color: Colors.green,
-                //margin: EdgeInsets.all(50.0),
-                //margin: EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0)
-                //margin: EdgeInsets.only(left: 50),
-                //margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                //padding: EdgeInsets.all(20),
-                //Layout widget(ROW COLUMN and Stack)
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                child: Text("Container 1"),
-                height: 100,
-                width: 200,
-                color: Colors.yellow,
-                //margin: EdgeInsets.all(50.0),
-                //margin: EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0)
-                //margin: EdgeInsets.only(left: 50),
-                //margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                //padding: EdgeInsets.all(20),
-                //Layout widget(ROW COLUMN and Stack)
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                child: Text("Container 1"),
-                height: 100,
-                width: 200,
-                color: Colors.red,
-                //margin: EdgeInsets.all(50.0),
-                //margin: EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0)
-                //margin: EdgeInsets.only(left: 50),
-                //margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                //padding: EdgeInsets.all(20),
-                //Layout widget(ROW COLUMN and Stack)
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(width: double.infinity)
-            ],
-          ),
+          child: Column(),
         ),
       ),
     );
   }
 }
+// child: Column(
+// // mainAxisSize: MainAxisSize.min,
+// mainAxisAlignment: MainAxisAlignment.center,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// //crossAxisAlignment: CrossAxisAlignment.center, //verticalDirection: VerticalDirection.up,
+// verticalDirection: VerticalDirection.down,
+// children: <Widget>[
+// Row(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// Container(
+// child: Text("Container R1"),
+// height: 100,
+// width: 100,
+// //width:double.infinity
+// color: Colors.cyan,
+// //margin: EdgeInsets.all(50.0),
+// //margin: EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0)
+// //margin: EdgeInsets.only(left: 50),
+// //margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+// //padding: EdgeInsets.all(20),//Layout widget(ROW COLUMN and Stack)
+// ),
+// SizedBox(
+// width: 10,
+// ),
+// Container(
+// child: Text("Container R2"),
+// height: 100,
+// width: 100,
+// color: Colors.pink,
+// //margin: EdgeInsets.all(50.0),
+// //margin: EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0)
+// //margin: EdgeInsets.only(left: 50),
+// //margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+// //padding: EdgeInsets.all(20),
+// //Layout widget(ROW COLUMN and Stack)
+// ),
+// SizedBox(
+// width: 10,
+// ),
+// Container(
+// child: Text("Container 1"),
+// height: 100,
+// width: 100,
+// color: Colors.orange,
+// //margin: EdgeInsets.all(50.0),
+// //margin: EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0)
+// //margin: EdgeInsets.only(left: 50),
+// //margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+// //padding: EdgeInsets.all(20),
+// //Layout widget(ROW COLUMN and Stack)
+// ),
+// ],
+// ),
+// SizedBox(
+// height: 10,
+// ),
+// Container(
+// child: Text("Container 1"),
+// height: 100,
+// width: 200,
+// //width:double.infinity
+// color: Colors.green,
+// //margin: EdgeInsets.all(50.0),
+// //margin: EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0)
+// //margin: EdgeInsets.only(left: 50),
+// //margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+// //padding: EdgeInsets.all(20),
+// //Layout widget(ROW COLUMN and Stack)
+// ),
+// SizedBox(
+// height: 10,
+// ),
+// Container(
+// child: Text("Container 1"),
+// height: 100,
+// width: 200,
+// color: Colors.yellow,
+// //margin: EdgeInsets.all(50.0),
+// //margin: EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0)
+// //margin: EdgeInsets.only(left: 50),
+// //margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+// //padding: EdgeInsets.all(20),
+// //Layout widget(ROW COLUMN and Stack)
+// ),
+// SizedBox(
+// height: 10,
+// ),
+// Container(
+// child: Text("Container 1"),
+// height: 100,
+// width: 200,
+// color: Colors.red,
+// //margin: EdgeInsets.all(50.0),
+// //margin: EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0)
+// //margin: EdgeInsets.only(left: 50),
+// //margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+// //padding: EdgeInsets.all(20),
+// //Layout widget(ROW COLUMN and Stack)
+// ),
+// SizedBox(
+// height: 10,
+// ),
+// Container(width: double.infinity)
+// ],
+// ),
