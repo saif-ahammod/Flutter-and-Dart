1bb00633a071acf6b92114e41591e16d3f2746dd
Md. Saif Ahammod Khan <saif.ahammod@northsouth.edu>
2022-10-13 23:56:16 +0600

    Template ready

--- dice/lib/main.dart
+++ dice/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+        backgroundColor: Colors.black54,
+        appBar: AppBar(
+          title: Text("Dice"),
+          backgroundColor: Colors.black,
+        )),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,14 +17,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black54,
-        appBar: AppBar(
-          title: Text("Dice"),
-          backgroundColor: Colors.black,
-        ),
-      ),
-    );
+    return Container();
   }
 }
