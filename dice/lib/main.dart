5fabfd99f10311754d79961b562224823ede945d
Md. Saif Ahammod Khan <saif.ahammod@northsouth.edu>
2022-11-10 15:42:42 +0600

    Dice image centered and padded

--- dice/lib/main.dart
+++ dice/lib/main.dart
@@ -19,15 +19,22 @@ class DicePage extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset("image/dice1.png"),
-        ),
-        Expanded(
-          child: Image.asset("image/dice2.png"),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.asset("image/dice1.png"),
+          )),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Image.asset("image/dice2.png"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
