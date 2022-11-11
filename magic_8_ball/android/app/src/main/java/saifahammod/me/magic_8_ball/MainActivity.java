f4a4b7c54ea7407d252e9f72f9c00a71b5a45563
Md. Saif Ahammod Khan <saif.ahammod@northsouth.edu>
2022-11-11 15:25:35 +0600

    Magic 8 ball project added

---
+++ magic_8_ball/android/app/src/main/java/saifahammod/me/magic_8_ball/MainActivity.java
@@ -0,0 +1,6 @@
+package saifahammod.me.magic_8_ball;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ magic_8_ball/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Magic8Ball());
+}
+
+class Magic8Ball extends StatefulWidget {
+  const Magic8Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Magic8Ball> createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
