c6b0470aff0e4aef6d937b7b5a57abdcee2ffe36
Md. Saif Ahammod Khan <saif.ahammod@northsouth.edu>
2022-11-12 21:50:03 +0600

    Project Xylophone 7 sound is working

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -6,24 +6,85 @@ void main() {
 }
 
 class XyloPhoneApp extends StatelessWidget {
-  const XyloPhoneApp({Key? key}) : super(key: key);
+  void playSound(int NodeNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$NodeNumber.wav'));
+  }
 
+  const XyloPhoneApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: FlatButton(
-            onPressed: () {
-              //   final player = AudioPlayer();
-              //   player.play(AssetSource('assets/note1.wav'));
-              final player = AudioPlayer();
-              player.play(AssetSource('note1.wav'));
-            },
-            child: Text("Click ME"),
-          ),
-        ),
+            child: Column(
+          children: [
+            FlatButton(
+              color: Colors.red,
+              onPressed: () {
+                //   final player = AudioPlayer();
+                //   player.play(AssetSource('assets/note1.wav'));
+                playSound(1);
+              },
+              child: Text(""),
+            ),
+            FlatButton(
+              color: Colors.orange,
+              onPressed: () {
+                //   final player = AudioPlayer();
+                //   player.play(AssetSource('assets/note1.wav'));
+                playSound(2);
+              },
+              child: Text(""),
+            ),
+            FlatButton(
+              color: Colors.yellow,
+              onPressed: () {
+                //   final player = AudioPlayer();
+                //   player.play(AssetSource('assets/note1.wav'));
+                playSound(3);
+              },
+              child: Text(""),
+            ),
+            FlatButton(
+              color: Colors.green,
+              onPressed: () {
+                //   final player = AudioPlayer();
+                //   player.play(AssetSource('assets/note1.wav'));
+                playSound(4);
+              },
+              child: Text(""),
+            ),
+            FlatButton(
+              color: Colors.lightBlueAccent,
+              onPressed: () {
+                //   final player = AudioPlayer();
+                //   player.play(AssetSource('assets/note1.wav'));
+                playSound(5);
+              },
+              child: Text(""),
+            ),
+            FlatButton(
+              color: Colors.blueAccent,
+              onPressed: () {
+                //   final player = AudioPlayer();
+                //   player.play(AssetSource('assets/note1.wav'));
+                playSound(6);
+              },
+              child: Text(""),
+            ),
+            FlatButton(
+              color: Colors.purple,
+              onPressed: () {
+                //   final player = AudioPlayer();
+                //   player.play(AssetSource('assets/note1.wav'));
+                playSound(7);
+              },
+              child: Text(""),
+            ),
+          ],
+        )),
       ),
     );
   }
