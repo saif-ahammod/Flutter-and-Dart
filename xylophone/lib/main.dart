14f55399f385b3c6a79e4e38e9866a7cd7c53df8
Md. Saif Ahammod Khan <saif.ahammod@northsouth.edu>
2022-11-12 15:10:43 +0600

    Project Xylophone Setup Complete

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const XyloPhoneApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class XyloPhoneApp extends StatelessWidget {
+  const XyloPhoneApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
   }
 }
