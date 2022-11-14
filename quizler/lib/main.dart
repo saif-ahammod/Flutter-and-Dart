51635906e02d74dde254251ed3b53a18bd337657
Md. Saif Ahammod Khan <saif.ahammod@northsouth.edu>
2022-11-14 23:59:32 +0600

    Project Quizzler bug fixed

--- quizler/lib/main.dart
+++ quizler/lib/main.dart
@@ -74,7 +74,7 @@ class _QuizPageState extends State<QuizPage> {
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
-                  color: Colors.white,
+                  color: Colors.blueAccent,
                 ),
               ),
             ),
