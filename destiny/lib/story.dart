24772eb86303684ee2ec298cc27d4de0d3846ae9
Md. Saif Ahammod Khan <saif.ahammod@northsouth.edu>
2022-11-17 23:43:15 +0600

    Destiny project - StoryBrain Class created

--- destiny/lib/story.dart
+++ destiny/lib/story.dart
@@ -2,7 +2,10 @@ class Story {
   late String storyTitle;
   late String choice1;
   late String choice2;
-  Story(String storyTitle, String choice1, String choice2) {
+  Story(
+      {required String storyTitle,
+      required String choice1,
+      required String choice2}) {
     this.storyTitle = storyTitle;
     this.choice1 = choice1;
     this.choice2 = choice2;
